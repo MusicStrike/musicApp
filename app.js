531428c9446352d488c45c289ff5898e4e11f9ca
samer <s.allaham@ltuc.com>
2020-01-26 17:29:56 +0200

    added the lyrics app

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var routes = require('./routes/routes.js');
+var path = require('path');
+
+var app=express();
+
+app.set('views/pages',path.join(__dirname,'views/pages'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
+
+app.use(bodyparser.json());
+app.use('/',routes);
+
+app.listen(2000,function(){
+  console.log('Server Started on Port 2000 ...');
+});
