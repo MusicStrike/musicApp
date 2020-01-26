60ce40f13a4628ce535fc6ca618d06b9abdf5e93
samer <s.allaham@ltuc.com>
2020-01-26 17:35:02 +0200

    cleaned code

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ const superagent = require('superagent');
 const PORT = process.env.PORT || 8080;
 const pg = require('pg')
 let app = express();
-// app.use(express.static('public'))
-//app.use('/static', express.static('public'))
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.get('/test', (req,res) =>{
-    // var request = require("request");
-    // let reqJson = req.body
-    // res.render('music/show')
+
 var options = {
   method: 'GET',
   url: `https://deezerdevs-deezer.p.rapidapi.com/search?q=${req.query.input}`,
@@ -27,38 +24,18 @@ var options = {
     'x-rapidapi-key': Music_API_KEY
   }
 }
-// superagent(options.url)
-// .then(musicData =>{
-//     let jSonData =musicData.body.data
-//     console.log(jSonData)
-//     console.log('hi');
-// })
+
 request(options, function (error, response, body) {
-   // console.log(options.qs.q)
+
     if (error) throw new Error(error)
- // superagent.get(options.url)
- //let jSonData =body.data
-    //console.log(jSonData)
-    
+
     
     let jSonData = JSON.parse(body)
     let song = jSonData.data.map(singer => new Music(singer))
-    // res.redirect('/')
+    
     res.render('pages/new' , {list:song}); 
 });
-    // for(let i=0 ;i<jSonData.data.length; i++){
-        
-    //     res.send(jSonData.data[i].title)
-    // }
-    // let rendered = jSonData.data.forEach(song => {
-    //     let formatted = new Music(song)
-    //     console.log(formatted)
-    // })
-    // let hello = jSonData.forEach(song => {
-    //     let formatted = new Music(song)
-    //     console.log(formatted)
-    // })
-     //res.render(`music/show` , hello)
+
   
 function Music(singer){
     this.title=singer.title
@@ -69,11 +46,11 @@ function Music(singer){
 } 
 })
 app.get('/', (req,res) => {
-    // console.log(req);
+  
      res.render('pages/index');
  })
 app.get('*', (req, res) => {
-   // console.log('this is for checking ', req);
+
     res.status(404).render('./pages/error', { erorr: '404 NOT FOUND' })
 });
 const client = new pg.Client(process.env.DATABASE_URL);
