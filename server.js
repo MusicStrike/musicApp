19b09c203265c34806e0758f1a983a54820c00c7
samer <s.allaham@ltuc.com>
2020-01-27 14:21:54 +0200

    merged and fixed conflictes with sheab

--- server.js
+++ server.js
@@ -71,204 +71,123 @@ function addtodatabase(req, res) {
     });
 }
 
-app.get('/test', (req, res) => {
-  // var request = require("request");
-  // let reqJson = req.body
-  // res.render('music/show')
+app.get('/', (req, res) => {
   var options = {
     method: 'GET',
-    url: `https://deezerdevs-deezer.p.rapidapi.com/search?q=${req.query.input}`,
-    qs: { q: req.query.input, },
-
+    url: 'https://30-000-radio-stations-and-music-charts.p.rapidapi.com/rapidapi',
+    qs: {chartsweek: '1'},
     headers: {
-      'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
-      'x-rapidapi-key': 'b7c9ddf4dcmsh3fed6859a77bf85p11dd80jsn396438f74ee5',
-    },
+      'x-rapidapi-host': '30-000-radio-stations-and-music-charts.p.rapidapi.com',
+      'x-rapidapi-key': 'eecd5fae1amsh533358a9cbb816dp1e354cjsn4ffd1b1a115c'
+    }
   };
-  // superagent(options.url)
-  // .then(musicData =>{
-  //     let jSonData =musicData.body.data
-  //     console.log(jSonData)
-  //     console.log('hi');
-  // })
+  
   request(options, function (error, response, body) {
-    // console.log(options.qs.q)
-    if (error) throw new Error(error);
-    // superagent.get(options.url)
-    //let jSonData =body.data
-    //console.log(jSonData)
+      if (error) throw new Error(error);
+  
+    
+   
+    var obj = JSON.parse(body);
+    console.log("this isnt",obj)
 
 
-    let jSonData = JSON.parse(body);
-    let song = jSonData.data.map(singer => new Music(singer));
-    // res.redirect('/')
-    res.render('pages/new', { list: song, });
+    let topSong = obj.results.map(song => new TopChart(song))
+    res.render('pages/index' , {topSong:topSong});
+
+    })
+    function TopChart(song){
+        console.log("this is in constructor",song.artist_song)
+      this.artist_song=song.artist_song
+      this.title_song=song.title_song
+    
+  } 
+  
   });
-  // for(let i=0 ;i<jSonData.data.length; i++){
-
-  //     res.send(jSonData.data[i].title)
-  // }
-  // let rendered = jSonData.data.forEach(song => {
-  //     let formatted = new Music(song)
-  //     console.log(formatted)
-  // })
-  // let hello = jSonData.forEach(song => {
-  //     let formatted = new Music(song)
-  //     console.log(formatted)
-  // })
-  //res.render(`music/show` , hello)
-
-  function Music(singer) {
-    this.title = singer.title;
-    this.preview_mp3 = singer.preview;
-    this.artistName = singer.artist.name;
-    this.album_cover_image = singer.album.cover_medium;
-    this.album_title = singer.album.title;
-  }
-});
-app.get('/', (req, res) => {
-  // console.log(req);
-  res.render('pages/index');
-});
-app.get('*', (req, res) => {
-  // console.log('this is for checking ', req);
-  res.status(404).render('./pages/error', { erorr: '404 NOT FOUND', });
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-app.get('/test', (req,res) =>{
-
-var options = {
-  method: 'GET',
-  url: `https://deezerdevs-deezer.p.rapidapi.com/search?q=${req.query.input}`,
-  qs: {q: req.query.input },
+// 'use strict';
+// require('dotenv').config();
+// const express = require('express');
+// const cors = require('cors');
+// const request = require('request');
+// let Music_API_KEY = process.env.Music_API_KEY;
+// const superagent = require('superagent');
+// const PORT = process.env.PORT || 5000;
+// const pg = require('pg')
+// let app = express();
+// app.use(express.static('public'))
+//app.use('/static', express.static('public'))
+// app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'))
+//ch2----
+// app.get('/test', (req,res) =>{
+    // var request = require("request");
+    // let reqJson = req.body
+    // res.render('music/show')
+// var options = {
+//   method: 'GET',
+//   url: `https://deezerdevs-deezer.p.rapidapi.com/search?q=${req.query.input}`,
+//   qs: {q: req.query.input },
   
-  headers: {
-    'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
-    'x-rapidapi-key': Music_API_KEY
-  }
-}
-
-request(options, function (error, response, body) {
-
-    if (error) throw new Error(error)
-
+//   headers: {
+//     'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
+//     'x-rapidapi-key': Music_API_KEY
+//   }
+// }
+// // superagent(options.url)
+// // .then(musicData =>{
+// //     let jSonData =musicData.body.data
+// //     console.log(jSonData)
+// //     console.log('hi');
+// // })
+// request(options, function (error, response, body) {
+//    // console.log(options.qs.q)
+//     if (error) throw new Error(error)
+//  // superagent.get(options.url)
+//  //let jSonData =body.data
+//     //console.log(jSonData)
     
-    let jSonData = JSON.parse(body)
-    let song = jSonData.data.map(singer => new Music(singer))
     
-    res.render('pages/new' , {list:song}); 
-});
-
+//     let jSonData = JSON.parse(body)
+//     let song = jSonData.data.map(singer => new Music(singer))
+    // res.redirect('/')
+    // ch1-----
+   // res.render('pages/new' , {list:song}); 
+// });
+    // for(let i=0 ;i<jSonData.data.length; i++){
+        
+    //     res.send(jSonData.data[i].title)
+    // }
+    // let rendered = jSonData.data.forEach(song => {
+    //     let formatted = new Music(song)
+    //     console.log(formatted)
+    // })
+    // let hello = jSonData.forEach(song => {
+    //     let formatted = new Music(song)
+    //     console.log(formatted)
+    // })
+//      res.render(`music/show` , hello)
   
-function Music(singer){
-    this.title=singer.title
-    this.preview_mp3=singer.preview
-    this.artistName=singer.artist.name
-    this.album_cover_image=singer.album.cover_medium
-    this.album_title=singer.album.title
-} 
-});
-
-//=============top chart============
-
-//=============================
-// var express = require('express');
-var bodyparser = require('body-parser');
-var routes = require('./routes/routes.js');
-var path = require('path');
-
-// var app=express();
-
-app.set('views/pages',path.join(__dirname,'views/pages'));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine','html');
-
-app.use(bodyparser.json());
-app.use(routes);
-
-
-app.get('/', (req, res) => {
-    var options = {
-      method: 'GET',
-      url: 'https://30-000-radio-stations-and-music-charts.p.rapidapi.com/rapidapi',
-      qs: {chartsweek: '1'},
-      headers: {
-        'x-rapidapi-host': '30-000-radio-stations-and-music-charts.p.rapidapi.com',
-        'x-rapidapi-key': 'eecd5fae1amsh533358a9cbb816dp1e354cjsn4ffd1b1a115c'
-      }
-    };
-    
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-    
-      
-     
-      var obj = JSON.parse(body);
-      console.log("this isnt",obj)
-
-
-      let topSong = obj.results.map(song => new TopChart(song))
-      res.render('pages/index' , {topSong:topSong});
-    //   obj.results.forEach(function(item){
-    //     console.log('123',item.artist_song,"song", item.title_song)
-    //     res.render(item.artist_song,"song", item.title_song)
-      })
-      function TopChart(song){
-          console.log("this is in constructor",song.artist_song)
-        this.artist_song=song.artist_song
-        this.title_song=song.title_song
-      
-    } 
-    
-    });
-
+// function Music(singer){
+//     this.title=singer.title
+//     this.preview_mp3=singer.preview
+//     this.artistName=singer.artist.name
+//     this.album_cover_image=singer.album.cover_medium
+//     this.album_title=singer.album.title
+// } 
 // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/', (req,res) => {
-  
+    // console.log(req);
      res.render('pages/index');
  })
-
 app.get('*', (req, res) => {
-
+   // console.log('this is for checking ', req);
     res.status(404).render('./pages/error', { erorr: '404 NOT FOUND' })
 });
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect(console.log('im DB'))
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Host : ${PORT}`);
-    });
-  });
+.then( () => {
+app.listen(PORT, () => {
+    console.log(`Host : ${PORT}` )
+})
+})
 
-function handleError(error, response) {
-  response.render('pages/error', { error: error, });
-}
