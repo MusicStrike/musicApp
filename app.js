617a6bb9157965dae4b7b172e1999f36ed56a4ae
samer <s.allaham@ltuc.com>
2020-01-27 16:44:09 +0200

    merged two JS files together

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var routes = require('./routes/routes.js');
-var path = require('path');
+// var express = require('express');
+// var bodyparser = require('body-parser');
+// var routes = require('./routes/routes.js');
+// var path = require('path');
 
-var app=express();
+// var app = express();
 
-app.set('views/pages',path.join(__dirname,'views/pages'));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine','html');
+// app.set('views/pages', path.join(__dirname, 'views/pages'));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
-app.use(bodyparser.json());
-app.use(routes);
+// app.use(bodyparser.json());
+// app.use(routes);
 
-app.listen(8080,function(){
-  console.log('Server Started on Port 8080 ');
-});
+
+// app.listen(8080,function(){
+//   console.log('Server Started on Port 8080 ');
+// });
--- server.js
+++ server.js
@@ -9,16 +9,29 @@ const superagent = require('superagent');
 const PORT = process.env.PORT || 5500;
 const pg = require('pg');
 let app = express();
-
+var bodyparser = require('body-parser');
+app.use(bodyparser.json());
+var routes = require('./routes/routes.js');
+app.use(routes);  
+var path = require('path');
+  // var app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true, }));
 app.use(express.static('public'));
+  
+  
+app.set('views/pages', path.join(__dirname, 'views/pages'));
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
 
 app.get('/myplaylist', addtodatabase);
 // app.get('/songs/show/:id', getsong);//when we want to view details
 app.get('/viewmylist', getsongs);
 app.get('/delete/:id', deletesong);
 app.get('/update/:id', updatesong);
+
+
+
 
 
 function deletesong(req, res) {
@@ -45,21 +58,13 @@ function getsongs(req, res) {
   let SQL = 'SELECT * FROM Songs;';
   client.query(SQL)
     .then(results => {
-      //   if (results.rows.rowCount === 0) {
-      //     res.render('pages/error');
-      //   } else {
+   
       res.render('pages/songs/show', { songs: results.rows, });
-      //   }
+     
     })
     .catch(err => handleError(err, res));
 }
-// function getsong(req, res) {
-//   let SQL = 'SELECT * FROM Songs WHERE id=$1;';
-//   let values = [request.params.id];
-//   client.query(SQL, values)
-//     .then(result => res.render('pages/songs/show', { song: result.rows[0], }))
-//     .catch(err => handleError(err, res));
-// }
+
 
 function addtodatabase(req, res) {
   let { title, preview_mp3, artistName, album_cover_image, album_title, } = req.query;
@@ -88,93 +93,29 @@ app.get('/', (req, res) => {
     
    
     var obj = JSON.parse(body);
-    console.log("this isnt",obj)
-
+    // console.log("this isnt",obj)
 
     let topSong = obj.results.map(song => new TopChart(song))
     res.render('pages/index' , {topSong:topSong});
 
     })
     function TopChart(song){
-        console.log("this is in constructor",song.artist_song)
+        // console.log("this is in constructor",song.artist_song)
       this.artist_song=song.artist_song
       this.title_song=song.title_song
     
   } 
   
   });
-// 'use strict';
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const request = require('request');
-// let Music_API_KEY = process.env.Music_API_KEY;
-// const superagent = require('superagent');
-// const PORT = process.env.PORT || 5000;
-// const pg = require('pg')
-// let app = express();
-// app.use(express.static('public'))
-//app.use('/static', express.static('public'))
-// app.set('view engine', 'ejs');
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'))
-//ch2----
-// app.get('/test', (req,res) =>{
-    // var request = require("request");
-    // let reqJson = req.body
-    // res.render('music/show')
-// var options = {
-//   method: 'GET',
-//   url: `https://deezerdevs-deezer.p.rapidapi.com/search?q=${req.query.input}`,
-//   qs: {q: req.query.input },
-  
-//   headers: {
-//     'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
-//     'x-rapidapi-key': Music_API_KEY
-//   }
-// }
-// // superagent(options.url)
-// // .then(musicData =>{
-// //     let jSonData =musicData.body.data
-// //     console.log(jSonData)
-// //     console.log('hi');
-// // })
-// request(options, function (error, response, body) {
-//    // console.log(options.qs.q)
-//     if (error) throw new Error(error)
-//  // superagent.get(options.url)
-//  //let jSonData =body.data
-//     //console.log(jSonData)
-    
-    
-//     let jSonData = JSON.parse(body)
-//     let song = jSonData.data.map(singer => new Music(singer))
-    // res.redirect('/')
-    // ch1-----
-   // res.render('pages/new' , {list:song}); 
-// });
-    // for(let i=0 ;i<jSonData.data.length; i++){
-        
-    //     res.send(jSonData.data[i].title)
-    // }
-    // let rendered = jSonData.data.forEach(song => {
-    //     let formatted = new Music(song)
-    //     console.log(formatted)
-    // })
-    // let hello = jSonData.forEach(song => {
-    //     let formatted = new Music(song)
-    //     console.log(formatted)
-    // })
-//      res.render(`music/show` , hello)
-  
-// function Music(singer){
-//     this.title=singer.title
-//     this.preview_mp3=singer.preview
-//     this.artistName=singer.artist.name
-//     this.album_cover_image=singer.album.cover_medium
-//     this.album_title=singer.album.title
-// } 
-// })
+  //====================================
+
+  // var express = require('express');
+
+
+
+
+
+
 app.get('/', (req,res) => {
     // console.log(req);
      res.render('pages/index');
