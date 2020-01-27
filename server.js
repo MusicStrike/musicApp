87af43b888419117a3f16efab88b9fddf51ec4b5
samer <s.allaham@ltuc.com>
2020-01-27 13:56:38 +0200

    merged div3 to staging

--- server.js
+++ server.js
@@ -48,7 +48,20 @@ function Music(singer){
 
 //=============top chart============
 
+//=============================
+// var express = require('express');
+var bodyparser = require('body-parser');
+var routes = require('./routes/routes.js');
+var path = require('path');
 
+// var app=express();
+
+app.set('views/pages',path.join(__dirname,'views/pages'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
+
+app.use(bodyparser.json());
+app.use(routes);
 
 
 app.get('/', (req, res) => {
